7c57014964dccd1730bad979b2ea151e276c0827
Giulia Chiola <giulia.chiola@modo.md>
2019-08-31 23:10:29 +0200

    feat: add styles

---
+++ src/components/copytext/component.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  label: 'A2',
+  variants: ['default.html'],
+  // General context, shared across all variants
+  context: {
+    copytext: {
+      text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.'
+    }
+  }
+}
---
+++ src/components/teaser/component.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  label: 'A5',
+  variants: ['default.html'],
+  context: {
+    button: {
+      text: 'Book your shuttle now!',
+      type: 'primary'
+    },
+    copytext: {
+      text: 'With our special offering you can enter one of the next spaceships. Get ready for your holiday behind the milky way!'
+    },
+    image: {
+      src: '/static/sample.png',
+      alt: 'A spaceship leaving planet earth'
+    },
+    heading: {
+      text: 'Holiday on Mars!',
+      type: 'teaser'
+    }
+  }
+}
