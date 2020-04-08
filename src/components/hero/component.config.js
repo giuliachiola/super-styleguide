538b11cd9b83604fedef53c64fa35b6364852277
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-04-08 21:28:14 +0200

    feat: update hero colors

--- src/components/hero/component.config.js
+++ src/components/hero/component.config.js
@@ -1,19 +1,39 @@
+const hero = {
+  title: `My awesome blog 🚀`,
+  subtitle: `This is where I write everything I think it'd be helpful to someone else<br>or - you know - the future me.`,
+}
+
 module.exports = {
   label: 'HE',
   variants: [
+    // Default
     {
       file: 'default.ejs',
       title: 'Default',
-      // label: 'HE-1',
       context: {
         uiContext: {
           containerModifiers: ['ui-container--medium-bg'],
           componentModifiers: [],
         },
         hero: {
-          title: `My awesome blog 🚀`,
-          subtitle: `This is where I write everything I think it'd be helpful to someone else<br>or - you know - the future me.`,
+          ...hero,
+          mainTag: 'git',
+        }
+      }
+    },
+    // Hero with image
+    {
+      file: 'default.ejs',
+      title: 'Hero with image',
+      context: {
+        uiContext: {
+          containerModifiers: ['ui-container--medium-bg'],
+          componentModifiers: [],
+        },
+        hero: {
+          ...hero,
           img: `https://images.unsplash.com/photo-1504237111663-37d6094bec09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80`,
+          credits: 'Giulia Chiola',
         }
       }
     },
