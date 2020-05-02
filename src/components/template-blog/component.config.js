377c4c5e2b6daf90249459e020fb7386a5d9f32e
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-05-02 16:56:31 +0200

    fix: #19, update header styles on mobile

--- src/components/template-blog/component.config.js
+++ src/components/template-blog/component.config.js
@@ -31,7 +31,7 @@ module.exports = {
     hero: {
       title: `Blog section`,
       subtitle: `This is where I write everything I think it'd be helpful to someone else<br>or - you know - the future me.`,
-      img: `https://images.unsplash.com/photo-1504237111663-37d6094bec09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80`,
+      // img: `https://images.unsplash.com/photo-1504237111663-37d6094bec09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3150&q=80`,
     }
   }
 }
